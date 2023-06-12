ad942214aa56b7d49fc9b7bbe3492895c099e8ed
nex <anttilajussi@outlook.com>
2023-06-12 13:35:43 +0300

    13_number_guess_game

--- react/13_number_guess_game/src/context/GameProvider.js
+++ react/13_number_guess_game/src/context/GameProvider.js
@@ -26,7 +26,7 @@ const GameProvider = (props) => {
             return;
         }
         const target = Math.floor(Math.random() * 100 ) + 1;
-        const message = "Hello "+name+". Guess a number between "+state.minimumGuess+" and "+state.maximumGuess+".";
+        const message = "Hello " + name + ". Guess a number between " + state.minimumGuess + " and " + state.maximumGuess + ".";
         setState((state) => {
             return {
                 ...state,
@@ -37,6 +37,78 @@ const GameProvider = (props) => {
         })
         navigate("/game");
     }
-}
 
+    const guess = (guess) => {
+        if(state.targetNumber === 0) {
+            setState({
+                targetNumber:0,
+                playerName:"",
+                noOfGuesses:0,
+                minimumGuess:1,
+                maximumGuess:100,
+                message:""
+            })
+            navigate("/");
+        }
+        if(isNaN(guess)) {
+            let message = "Please enter a NUMBER between " + state.minimumGuess + " and " + state.maximumGuess + ".";
+            setState((state) => {
+                return {
+                    ...state,
+                    message:message
+                }
+            })
+        }
+        let tempGuess = parseInt(guess);
+        if(tempGuess < state.minimumGuess || tempGuess > state.maximumGuess) {
+            let message = "Please enter a number between " + state.minimumGuess + " and " + state.maximumGuess + ".";
+            setState((state) => {
+                return {
+                    ...state,
+                    message:message
+                }
+            })
+            return;
+        }
+        if(tempGuess < state.targetNumber && tempGuess >= state.minimumGuess) {
+            let message = "You guessed too low. Guess between " + guess + " and " + state.maximumGuess + ".";
+            setState((state) => {
+                return {
+                    ...state,
+                    noOfGuesses:state.noOfGuesses+1,
+                    minimumGuess:guess,
+                    message:message
+                }
+            })
+            return;
+        }
+        if(tempGuess > state.targetNumber && tempGuess <= state.maximumGuess) {
+            let message = "You guessed too high. Guess between " + state.minimumGuess + " and " + guess + ".";
+            setState((state) => {
+                return {
+                    ...state,
+                    noOfGuesses:state.noOfGuesses+1,
+                    maximumGuess:guess,
+                    message:message
+                }
+            })
+            return;
+        }
+        if(tempGuess === state.targetNumber) {
+            let noOfGuesses = state.noOfGuesses+1;
+            alert("Congrats " + state.playerName + "! You won with " + noOfGuesses + " guesses.");
+            setState({
+                playerName:"",
+                noOfGuesses:0,
+                targetNumber:0,
+                maximumGuess:100,
+                minimumGuess:1,
+                message:""
+            })
+            navigate("/");
+            return;
+        }
+        console.log("Should not come here");
+    }
+}
 export default GameProvider;
