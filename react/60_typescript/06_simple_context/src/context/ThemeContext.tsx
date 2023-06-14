eadaf077441a64db409d084d541964684655a786
nex <anttilajussi@outlook.com>
2023-06-14 14:37:17 +0300

    06_simple_context

---
+++ react/60_typescript/06_simple_context/src/context/ThemeContext.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export interface ThemeType {
+    color:string,
+    backgroundColor:string;
+}
+
+export const themes = {
+    light:{
+        color:"#000000",
+        backgroundColor:"#d3d3d3"
+    },
+    dark:{
+        color:"#FFFFFF",
+        backgroundColor:"#595959"
+    }
+}
+
+const ThemeContext = React.createContext<ThemeType>(themes.dark);
+
+export default ThemeContext;
