c6c59b5f3413c8525497f9a56a3dddfc038e1b04
nex <anttilajussi@outlook.com>
2023-06-14 10:50:34 +0300

    60_typescript

---
+++ react/60_typescript/01_hello_world/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld name="Nex"/>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
