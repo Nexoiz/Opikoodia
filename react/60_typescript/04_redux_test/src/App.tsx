edca148bc3b6cc2a3ec71408113741c132c0ac71
nex <anttilajussi@outlook.com>
2023-06-14 13:22:48 +0300

    04_redux_test

---
+++ react/60_typescript/04_redux_test/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm/>
+      <hr/>
+      <ContactList/>
+    </div>
+  );
+}
+
+export default App;
